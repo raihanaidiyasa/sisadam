ef04e02dc227411d655790bbaabbf8a9c914cbdf
nfzmhn <zidninurfauzi2203@gmail.com>
2025-06-11 17:18:40 +0700

    update blade fe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{ asset('dashboard_public/dashboard_public.css') }}">
+    <link rel="stylesheet" href="{{ asset('navbar/navbar_footer.css') }}">
     
     @yield('styles') {{-- Untuk CSS tambahan spesifik halaman jika ada --}}
 </head>
